a29e138b5e717d4f0007ff6a2ccffbc0fb73c5d0
fahmaz <ashrfhmz777@gmail.com>
2024-02-12 17:25:41 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'sans': ['Montserrat', 'sans-serif'],
+      'inter':['Inter','sans-serif']
+      // You can define other font families here if needed
+    },
+    extend: {},
+  },
+  plugins: [],
+}
